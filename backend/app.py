8d2e3db1b4f4e3aca1832062ef50a4fe7709a5e1
MilesMa <miles.ma@oneworldtrip.cn>
2026-09-11 15:37:28 +0800

    fix: isolate data center service ports

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, debug=app.config["DEBUG"])
-
+    app.run(host="0.0.0.0", port=5060, debug=app.config["DEBUG"])
